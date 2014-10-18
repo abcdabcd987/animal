d24a29154c9bd415dd580b265a9b2f2509fd09fa
juda <judaplus@sjtu.edu.cn>
2014-10-18 18:33:59 +0800

    fixed socket error

--- server/network.py
+++ server/network.py
@@ -14,32 +14,35 @@ class server:
     network communication by using socket
     '''
     def __init__(self,log):
-        host=socket.gethostname()
+        host=socket.gethostbyname(socket.gethostname())
         port=12345
         self.log=log
         self.spy=socket.socket()
         self.spy.bind((host,port))
         self.spy.listen(2)        
         
-        self.log.logging("wating to connect ...",'SHOWALL')        
+        self.log.logging("wating to connect ...",'SHOWALL')
+        self.log.logging("The PC's host is %s"%host,'SHOWALL')
         
         self.AI=[None,None]
         self.AIname=[None,None]        
         
-        self.AI[0],addr=self.spy.accept()
-        self.log.logging("%s connected"%addr,'SHOWALL')
-        self.AI[1],addr=self.spy.accept()
-        self.log.logging("%s connected"%addr,'SHOWALL')
-        
         #determine which player is first player
+        first=0
         if random.random()<0.5:
-            self.AI[0],self.AI[1]=self.AI[1],self.AI[0]
+            first=1
+
+        self.AI[first],addr=self.spy.accept()
+        self.log.logging("%s connected"%addr[0],'SHOWALL')
+        self.AI[1-first],addr=self.spy.accept()
+        self.log.logging("%s connected"%addr[0],'SHOWALL')
+
         self.send(self.AI[0],'0')
         self.AIname[0]=self.recieve(self.AI[0]).strip()
         self.send(self.AI[1],'1')
         self.AIname[1]=self.recieve(self.AI[1]).strip()
-        self.log.loging("%s is the first player"%self.AIname[0],'SHOWALL')
-        self.log.loging("%s is the second player"%self.AIname[1],'SHOWALL')
+        self.log.logging("%s is the first player"%self.AIname[0],'SHOWALL')
+        self.log.logging("%s is the second player"%self.AIname[1],'SHOWALL')
         
         
     def send(self,client,message):
